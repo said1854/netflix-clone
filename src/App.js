4489c21f1ad52c70461e47ea6b2303f416922c11
said1854 <msaliogullari@gmail.com>
2022-10-25 23:10:14 +0300

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './Netflix-Logo.wine.png';
+import Navbar from './components/Nabvar';
 
 
 function App() {
   return (
     <>
-  <p className="text-white border-double mx-auto "> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa quo, corporis tempore omnis asperiores placeat amet laboriosam cum quasi sunt aperiam animi nesciunt deleniti aliquid harum, commodi assumenda velit odio voluptatum in, nisi nulla. Cupiditate asperiores omnis, nisi in modi autem aspernatur atque nesciunt nobis sed odit vel nemo soluta accusantium provident fugiat voluptatem mollitia ad error magnam officiis explicabo debitis architecto. Temporibus quasi ducimus provident ipsa nostrum sit repellendus corporis voluptates numquam explicabo! Doloremque fugiat obcaecati inventore aperiam esse neque accusantium, veniam libero nulla iste nobis molestiae veritatis ea saepe maiores voluptate optio eos dolorum officia minus facilis fuga! </p>
+    <h1 className="text-white">Hello from App</h1>
+    <Navbar />
+
     </>
   );
 }
---
+++ src/components/Nabvar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Nabvar = () => {
+  return (
+    <>
+        <h1 className='text-white border-r-zinc-50'>
+            Hello from Navbar 
+        </h1>
+    </>
+    )
+}
+
+export default Nabvar;
