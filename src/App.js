d182900092e789b938a1d3a6b11d86ea4137251a
said1854 <msaliogullari@gmail.com>
2022-10-25 20:35:43 +0300

    configuration completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './Netflix-Logo.wine.png';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+  <p className="text-white border-double mx-auto "> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa quo, corporis tempore omnis asperiores placeat amet laboriosam cum quasi sunt aperiam animi nesciunt deleniti aliquid harum, commodi assumenda velit odio voluptatum in, nisi nulla. Cupiditate asperiores omnis, nisi in modi autem aspernatur atque nesciunt nobis sed odit vel nemo soluta accusantium provident fugiat voluptatem mollitia ad error magnam officiis explicabo debitis architecto. Temporibus quasi ducimus provident ipsa nostrum sit repellendus corporis voluptates numquam explicabo! Doloremque fugiat obcaecati inventore aperiam esse neque accusantium, veniam libero nulla iste nobis molestiae veritatis ea saepe maiores voluptate optio eos dolorum officia minus facilis fuga! </p>
+    </>
   );
 }
 
