f9c06b13d2b3407fb330754dacc04e86b8fc18f6
said1854 <msaliogullari@gmail.com>
2022-10-26 17:17:49 +0300

    rows and horizontal scrool completed

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,11 +1,12 @@
 import axios from "axios";
 import React from "react";
 import Movie from "./Movie";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
 import { useEffect } from "react";
 import { useState } from "react";
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, rowId }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -13,16 +14,39 @@ function Row({ title, fetchUrl }) {
       setMovies(res.data.results);
     });
   }, [fetchUrl]);
-  //   console.log(movies);
+
+  const slideLeft = () => {
+    const slider = document.getElementById("slider" + rowId);
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+
+  const slideRight = () => {
+    const slider = document.getElementById("slider" + rowId);
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
+
   return (
     <>
       <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
-      <div className="relative flex items-center">
-        <div id={"slider"}>
+      <div className="relative flex items-center group">
+        <MdChevronLeft
+          onClick={slideLeft}
+          className="left-0 bg-white rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+          size={40}
+        />
+        <div
+          id={"slider" + rowId}
+          className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative"
+        >
           {movies.map((item, id) => (
-            <Movie item={item} key={id} />
+            <Movie id={rowId} item={item} key={id} />
           ))}
         </div>
+        <MdChevronRight
+          onClick={slideRight}
+          className="right-0 bg-white rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+          size={40}
+        />
       </div>
     </>
   );
